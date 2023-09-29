6968f322d9d5d042d5b7a1521daafeb161a25540
andreastif <45339645+andreastif@users.noreply.github.com>
2023-09-29 09:26:10 +0200

    Initial commit

---
+++ src/test/java/com/gruppuppgift3/Gruppuppgift3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppuppgift3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppuppgift3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
